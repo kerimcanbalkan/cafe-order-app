c6bb0d6dfcc1765401d4cb371b300c7a7b188d66
kerimcanbalkan <kerimcanbalkan@gmail.com>
2025-03-03 17:51:04 +0300

    refactor: Changed menuNavbar logo to Cafe

--- src/components/MenuNavbar.jsx
+++ src/components/MenuNavbar.jsx
@@ -1,11 +1,18 @@
-
 function MenuNavbar() {
   return (
     <div className="container mx-auto sticky top-0 z-50 bg-white p-4 flex justify-between items-center">
-      <h1 className="text-nord-0 text-2xl">LOGO</h1>
-        <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" className="fill-nord-0 cursor-pointer hover:fill-nord-2"><path d="M221-120q-27 0-48-16.5T144-179L42-549q-5-19 6.5-35T80-600h190l176-262q5-8 14-13t19-5q10 0 19 5t14 13l176 262h192q20 0 31.5 16t6.5 35L816-179q-8 26-29 42.5T739-120H221Zm-1-80h520l88-320H132l88 320Zm260-80q33 0 56.5-23.5T560-360q0-33-23.5-56.5T480-440q-33 0-56.5 23.5T400-360q0 33 23.5 56.5T480-280ZM367-600h225L479-768 367-600Zm113 240Z"/></svg>
+      <h1 className="text-nord-0 text-2xl">CAFE</h1>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        height="24px"
+        viewBox="0 -960 960 960"
+        width="24px"
+        className="fill-nord-0 cursor-pointer hover:fill-nord-2"
+      >
+        <path d="M221-120q-27 0-48-16.5T144-179L42-549q-5-19 6.5-35T80-600h190l176-262q5-8 14-13t19-5q10 0 19 5t14 13l176 262h192q20 0 31.5 16t6.5 35L816-179q-8 26-29 42.5T739-120H221Zm-1-80h520l88-320H132l88 320Zm260-80q33 0 56.5-23.5T560-360q0-33-23.5-56.5T480-440q-33 0-56.5 23.5T400-360q0 33 23.5 56.5T480-280ZM367-600h225L479-768 367-600Zm113 240Z" />
+      </svg>
     </div>
-  )
-} 
+  );
+}
 
-export {MenuNavbar}
+export { MenuNavbar };
