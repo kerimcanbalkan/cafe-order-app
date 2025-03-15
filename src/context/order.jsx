a3dbb5dde8f35048aa1e8e8d615e5b8c81c82c41
kerimcanbalkan <kerimcanbalkan@gmail.com>
2025-03-15 16:12:47 +0300

    fix: Active order total price calculation

--- src/context/order.jsx
+++ src/context/order.jsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext } from "react";
 import { useQuery } from "@tanstack/react-query";
 import { getActiveOrder } from "../api/order";
 import { useParams } from "react-router-dom";
@@ -7,39 +7,19 @@ export const OrderContext = createContext();
 
 export const OrderProvider = ({ children }) => {
   const { tableNumber } = useParams();
-  const [totalPrice, setTotalPrice] = useState(0);
   
   // Fetch active order from database
   const { data: order = [], refetch } = useQuery({
     queryKey: ["order", tableNumber],
     queryFn: async () => {
       const response = await getActiveOrder(tableNumber);
-      
-      let calculatedTotalPrice = 0;
-      const transformedOrder = response.data?.length > 0
-        ? response.data.reduce((acc, order) => {
-            calculatedTotalPrice += order.totalPrice;
-            order.items.forEach((item) => {
-              const existingItem = acc.find((i) => i.menuItem.id === item.menuItem.id);
-              if (existingItem) {
-                existingItem.quantity += item.quantity;
-              } else {
-                acc.push({ ...item });
-              }
-            });
-            return acc;
-          }, [])
-        : [];
-      
-      // Set totalPrice after calculation
-      setTotalPrice(calculatedTotalPrice);
-
-      return transformedOrder;
+      console.log("This is order", response.data);
+      return response?.data || [];
     },
   });
 
   const getOrderTotal = () => {
-    return Math.trunc(totalPrice * 100) / 100;
+    return Math.trunc(order.reduce((acc,item) => acc +item.totalPrice, 0) * 100) / 100;
   };
 
   return (
