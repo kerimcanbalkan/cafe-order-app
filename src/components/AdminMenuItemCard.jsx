41d334aae96a81646ec35c20a4dd538487c4f9f8
kerimcanbalkan <kerimcanbalkan@gmail.com>
2025-04-07 22:05:41 +0300

    feat: Menu item deletion implementation

--- src/components/AdminMenuItemCard.jsx
+++ src/components/AdminMenuItemCard.jsx
@@ -16,6 +16,10 @@ import {
 } from "@/components/ui/alert-dialog"
 import { Trash, Pencil } from "lucide-react";
 import { useState } from "react";
+import { useMutation } from "@tanstack/react-query";
+import { deleteMenuItem } from "@/api/menu";
+import Loading from "@/components/Loading";
+import { useAlert } from "@/components/AlertProvider";
 
 /**
  * Renders a clickable menuItem card that opens a details dialog.
@@ -27,7 +31,7 @@ import { useState } from "react";
  * @param {number} props.menuItem.price - The menuItem price.
  * @param {string} props.menuItem.image - The menuItem image url .
  */
-export default function AdminMenuItemCard({ menuItem }) {
+export default function AdminMenuItemCard({ menuItem, refetch }) {
   const [open, setOpen] = useState(false);
   
   return (
@@ -59,25 +63,49 @@ export default function AdminMenuItemCard({ menuItem }) {
           </div>
         </CardContent>
       </Card>
-      <DeleteModal open={open} setOpen={setOpen} itemName={menuItem.name}/>
+      <DeleteModal open={open} setOpen={setOpen} menuItem={menuItem} refetch={refetch}/>
     </>
   );
 }
 
 
-const DeleteModal = ({open, setOpen, itemName}) => {
+const DeleteModal = ({open, setOpen, menuItem, refetch}) => {
+  const showAlert = useAlert();
+  const token = localStorage.getItem("authToken");
+  
+  const mutation = useMutation({
+    mutationFn: ({token, id}) => {
+      return deleteMenuItem({token, id});
+    },
+    onError: (error) => {
+      console.error("Error deleting item:", error);
+      showAlert("error", "Error!", "Could delete the item");
+    },
+    onSuccess: () => {
+      refetch();
+      showAlert("success", "Success!", "Item deleted succesfully");
+    },
+  });
+
+  const handleDeleteClick = () => {
+    mutation.mutate({token: token, id: menuItem.id});
+    console.log("Deleting "+ menuItem.name + " "+ menuItem.id);
+  }
+  
   return (
     <AlertDialog open={open} onOpenChange={setOpen}>
       <AlertDialogContent>
         <AlertDialogHeader>
-          <AlertDialogTitle>Are you sure you want to delete <span className="text-nord-11">{itemName}</span></AlertDialogTitle>
+          <AlertDialogTitle>Are you sure you want to delete <span className="text-nord-11">{menuItem.name}</span></AlertDialogTitle>
           <AlertDialogDescription>
            This action cannot be undone. This will permanently delete your menu item.
           </AlertDialogDescription>
         </AlertDialogHeader>
         <AlertDialogFooter>
           <AlertDialogCancel className="text-white bg-nord-14 border-0 hover:bg-nord-14 hover:text-white">Cancel</AlertDialogCancel>
-          <AlertDialogAction className="text-white bg-nord-11 hover:bg-nord-11">Delete</AlertDialogAction>
+          <AlertDialogAction className="text-white bg-nord-11 hover:bg-nord-11" onClick={handleDeleteClick}>
+                  {mutation.isPending ? <Loading/> : 'Delete'}
+          </AlertDialogAction>
         </AlertDialogFooter>
       </AlertDialogContent>
     </AlertDialog>
