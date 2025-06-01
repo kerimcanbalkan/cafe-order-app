874e395951b49e81b9ec226965271127eee0c132
kerimcanbalkan <kerimcanbalkan@gmail.com>
2025-06-01 17:24:08 +0300

    feat: add TableCard design and display tables on cashier page

---
+++ src/components/TableCard.jsx
@@ -0,0 +1,75 @@
+import {
+  Card,
+  CardContent,
+  CardTitle,
+} from "@/components/ui/card";
+import { useQuery } from "@tanstack/react-query";
+import { getActiveOrder } from "@/api/order";
+import Loading from "@/components/Loading";
+import { Button } from "@/components/ui/button";
+
+
+/**
+ * Renders a clickable tabel card that shows order status of the table
+ * @param {Object} props - The component props.
+ * @param {Object} props.table - table object with table details
+ * @param {string} props.table.id - The table ID.
+  * @param {string} props.table.name - The table name.
+ */
+export default function TableCard({ table }){
+
+  const {
+    data: order,
+    isLoading,
+    error,
+    refetch,
+  } = useQuery({
+    queryKey: ["order", table.id],
+    queryFn: () => getActiveOrder(table.id),
+  })
+
+  const orders = order?.data || [];
+
+  const handleClick = () => {
+    console.log(table.name,
+      " ", orders);
+  }
+
+
+  if (isLoading)
+    return (
+      <Card className="cursor-pointer">
+        <CardContent className="flex items-center justify-center p-4 h-30">
+          <CardTitle className="text-nord-0">
+            <Loading />
+          </CardTitle>
+        </CardContent>
+      </Card>
+    );
+  
+  if (error)
+    return (
+      <Card className="cursor-pointer">
+        <CardContent className="flex flex-col items-center justify-center p-4 h-30 gap-3">
+          <Button
+            className="bg-0 border-1 border-nord-11 hover:bg-nord-1 text-nord-11 text-xs rounded-lg active:scale-95 hover:bg-nord-11 hover:text-white transition"
+            onClick={() => {
+              refetch();
+            }}
+          >
+             Retry
+          </Button>
+        </CardContent>
+      </Card>
+    );
+  
+  return (
+    <Card className={`cursor-pointer ${orders.length === 0 ? "" : "shadow-nord-11"}`} onClick={handleClick}>
+      <CardContent className="flex items-center justify-center p-4 min-h-30">
+        <CardTitle className="text-nord-0">
+          {table.name}
+        </CardTitle>
+      </CardContent>
+    </Card>
+  )
+}
--- src/pages/Cashier.jsx
+++ src/pages/Cashier.jsx
@@ -1,5 +1,53 @@
+import TableCard from "@/components/TableCard";
+import Loading from "@/components/Loading";
+import { Button } from "@/components/ui/button";
+import { getTables } from "@/api/table";
+import { useQuery } from "@tanstack/react-query";
+
 export default function Cashier() {
+    const {
+    data: table,
+    isLoading,
+    error,
+    refetch,
+  } = useQuery({
+    queryKey: ["table"],
+    queryFn: getTables,
+  });
+
+  if (isLoading)
+    return (
+      <div>
+        <Loading />
+      </div>
+    );
+  
+  if (error)
+    return (
+      <div className="container mx-auto h-svh">
+        <div className="w-full h-full flex flex-col gap-3 items-center justify-center">
+          <h3 className="text-nord-11 text-lg">Could not load the tables!</h3>
+          <Button
+            className="bg-nord-11 hover:bg-nord-1 text-white rounded-lg active:scale-95 transition"
+            onClick={() => {
+              refetch();
+            }}
+          >
+             Retry
+          </Button>
+        </div>
+      </div>
+    );
+
+  const tables = table?.data || [];
+  
   return (
-  <div>Cashier</div>
-  )
+    <div className="mx-auto mt-10">
+      <div className="container mx-auto my-5 grid grid-cols-3 sm:grid-cols-5 lg:grid-cols-6 gap-2">
+        {tables.map((table, index) => (
+          <TableCard key={index} table={table}/>
+        ))}
+      </div>
+    </div>
+  );
 }
